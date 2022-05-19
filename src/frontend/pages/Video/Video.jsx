64b40a83c8d04a868ccee53bbfc5622664d4eda6
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-05-19 11:42:10 +0530

    fix : page not found and videos onclick

--- src/frontend/pages/Video/Video.jsx
+++ src/frontend/pages/Video/Video.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import { useParams } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -31,6 +31,10 @@ export const Video = () => {
   const { addToHistoryVideos } = useHistory();
   const singleVideo = videos.find((video) => video._id === videoId);
   const { _id, views, video, title, creator, description } = singleVideo;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
